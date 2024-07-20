8fdba736c03319e427cb762c2d7db8a70fa002a4
MarcosGray <marcosgray@gmail.com>
2024-07-20 09:15:18 -0300

    Construction of the header and task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 
-import './App.module.css'
+import { PlusCircle } from '@phosphor-icons/react'
+import { Header } from './components/Header'
+import Logo from './assets/Clipboard.svg'
+import styles from './App.module.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Todo List</h1>
-    </>
+    <main>
+      <Header />
+      <section className={styles.content} >
+        <div className={styles.newTaskContainer} >
+          <input className={styles.newTaskContainer} placeholder='Adicione aqui uma nova tarefa' />
+          <button className={styles.newTaskContainer}>
+            Criar
+            <PlusCircle size={16} color='#f2f2f2' weight='bold' />
+          </button>
+        </div>
+        <div className={styles.tasksList} >
+          <div>
+            <header className={styles.containerHearderList}>
+              <aside>
+                <p>Tarefas criadas</p>
+                <span>0</span>
+              </aside>
+              <aside>
+                <p>Concluídas</p>
+                <span>0</span>
+              </aside>
+            </header>
+          </div>
+          <div className={styles.containerEmpty}>
+            <img src={Logo} alt='icone de prancheta' />
+            <p>
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              Crie tarefas e organize seus itens a fazer
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
